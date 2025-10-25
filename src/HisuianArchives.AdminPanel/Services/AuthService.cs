c001e025fb88b88c3cb720ef5d9e6344ab7e6a27
FocaChu <joaoguttocordeiro@gmail.com>
2025-10-25 01:57:42 -0300

    feat(admin): added auth service

--- src/HisuianArchives.AdminPanel/Services/AuthService.cs
+++ src/HisuianArchives.AdminPanel/Services/AuthService.cs
@@ -1,5 +1,32 @@
-﻿namespace HisuianArchives.AdminPanel.Services;
+﻿using HisuianArchives.Application.DTOs.Auth; 
+
+namespace HisuianArchives.AdminPanel.Services;
 
 public class AuthService
 {
+    private readonly HttpClient _httpClient;
+
+    public AuthService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("HisuianApi");
+    }
+
+    public async Task<AuthResponseDto?> LoginAsync(LoginRequestDto loginRequest)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/auth/login", loginRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<AuthResponseDto>();
+            }
+
+            return null;
+        }
+        catch (HttpRequestException)
+        {
+            return null;
+        }
+    }
 }
